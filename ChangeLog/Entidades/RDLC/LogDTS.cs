147672c41035adfb23f08b3cf0f525a2a383141b
Luiz Henrique Gioda Finger <rikefinger@hotmail.com>
2021-11-16 13:34:01 -0300

    Commit

--- ChangeLog/Entidades/RDLC/LogDTS.cs
+++ ChangeLog/Entidades/RDLC/LogDTS.cs
@@ -1,59 +1,21 @@
-﻿using System;
+﻿using Entidades.Enumeradores;
+using System;
 
-namespace Entidades.Suprimentos.Reports
+namespace Entidades.RDLC
 {
    public class LogDTS
    {
-      #region Variaveis
+      public string Titulo { get; set; }
 
-      public int CodigoNota { get; set; }
+      public string Descricao { get; set; }
 
-      public int CodigoConta { get; set; }
+      public string Sprint { get; set; }
 
-      public int ParcelaFatura { get; set; }
+      public string LoginUsuario { get; set; }
 
-      public int? CodigoCaixa { get; set; }
+      public Tipo Tipo { get; set; }
 
-      public int? CodigoEventoSocial { get; set; }
+      public DateTime DataHora { get; set; }
 
-      public long MatriculaFornecedor { get; set; }
-
-      public decimal ValorNota { get; set; }
-
-      public decimal ValorFatura { get; set; }
-
-      public decimal ValorConta { get; set; }
-
-      public decimal ValorTotalFatura { get; set; }
-
-      public string NumeroNota { get; set; }
-
-      public string NomeFornecedor { get; set; }
-
-      public string DescricaoEventoSocial { get; set; }
-
-      public string DescricaoConta { get; set; }
-
-      public string Observacoes { get; set; }
-
-      public string StringParcelaFatura { get; set; }
-
-      public string SituacaoFatura { get; set; }
-
-      public string DataPagamentoFatura { get; set; }
-
-      public string TipoFormaPagamento { get; set; }
-
-      #endregion
-
-      #region Datas
-
-      public DateTime DataEmissao { get; set; }
-
-      public DateTime DataCompetencia { get; set; }
-
-      public DateTime DataVencimentoFatura { get; set; }
-
-      #endregion
    }
 }
